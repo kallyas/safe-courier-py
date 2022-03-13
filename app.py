bbfa7bdad03a9a286d7863cc110ef72143d54017
kallyas <kallyasmedia@gmail.com>
2022-03-13 22:28:53 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from api import create_app, db, jwt
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    db.create_all(app=create_app())
+    app.run()
