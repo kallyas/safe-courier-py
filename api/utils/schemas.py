ea762bfdcb6bc64602ed305681f5aeff7aad0237
kallyas <kallyasmedia@gmail.com>
2022-03-16 02:10:36 +0300

    ch: add model schemas

---
+++ api/utils/schemas.py
@@ -0,0 +1,18 @@
+from api import ma
+from marshmallow_sqlalchemy import ModelSchema
+from models import Parcel, User
+
+class UserSchema(ModelSchema):
+    class Meta:
+        fields = ('id', 'username', 'email', 
+                    'address', 'phone_number',
+                    'first_name', 'last_name',
+                    'role_id'
+                    )
+
+
+class ParcelSchema(ModelSchema):
+    class Meta:
+        fields = ('id', 'user_id', 'name', 'description', 'weight', 'price', 'status', 'location', 'destination', 'created_at', 'updated_at')
+        model = Parcel
+
