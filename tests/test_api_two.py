5f30a52ae40ce4f1cb450295f888598617c065c9
kallyas <kallyasmedia@gmail.com>
2022-03-20 20:55:02 +0300

    ch: add parcel create test

--- tests/test_api_two.py
+++ tests/test_api_two.py
@@ -11,17 +11,38 @@ user = {
     'address': 'test'
 }
 
+parcel = {
+    'name': 'Test Parcel',
+    'description': 'test description',
+    'weight': 2,
+    'price': 200,
+    'location': 'test location',
+    'destination': 'test destination',
+    'status': 'pending',
+    'current_location': 'test current location'
+}
 
-class TestAuth(BaseCase):
+headers = {'Content-Type': 'application/json'}
+
+
+class APITests(BaseCase):
     def register_user(self):
-        headers = {'Content-Type': 'application/json'}
         response = self.app.\
             post('/api/v1/auth/register',
                  data=json.dumps(user),
                  headers=headers)
 
+    def login_user(self):
+        response = self.app.\
+            post('/api/v1/auth/login', 
+                    data=json.dumps({
+                        "email": "test@gmail.com",
+                        "password": "test"
+                    }), headers=headers)
+                    
+        return json.loads(response.data)['access_token']
+
     def test_register_success(self):
-        headers = {'Content-Type': 'application/json'}
         response = self.app.\
             post('/api/v1/auth/register', data=json.dumps(user), headers=headers)
         self.assertEqual(response.status_code, 201)
@@ -35,7 +56,7 @@ class TestAuth(BaseCase):
                      'email': 'test@gmail.com',
                      'password': 'test'
                  }),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', str(response.data))
 
@@ -44,7 +65,7 @@ class TestAuth(BaseCase):
         response = self.app.\
             post('/api/v1/auth/login',
                  data=json.dumps({}),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 400)
 
     def test_login_fail_no_email(self):
@@ -54,7 +75,7 @@ class TestAuth(BaseCase):
                  data=json.dumps({
                      'password': 'test'
                  }),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 400)
 
     def test_login_fail_no_password(self):
@@ -64,7 +85,7 @@ class TestAuth(BaseCase):
                  data=json.dumps({
                      'email': 'test@gmail.com'
                  }),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 400)
 
     def test_login_fail_wrong_email(self):
@@ -75,7 +96,7 @@ class TestAuth(BaseCase):
                      'email': 'tes@gmail.com',
                      'password': 'test'
                  }),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 404)
         self.assertIn('User by tes@gmail.com not found', str(response.data))
 
@@ -87,7 +108,7 @@ class TestAuth(BaseCase):
                      'email': 'test@gmail.com',
                      'password': 'test1'
                  }),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 401)
         self.assertIn('Invalid credentials', str(response.data))
 
@@ -95,5 +116,17 @@ class TestAuth(BaseCase):
         response = self.app.\
             post('/api/v1/auth/register',
                  data=json.dumps({}),
-                 headers={'Content-Type': 'application/json'})
+                 headers=headers)
         self.assertEqual(response.status_code, 400)
+
+    def test_create_parcel_success(self):
+        self.register_user()
+        self.access_token = self.login_user()
+        headers['Authorization'] = 'Bearer ' + self.access_token
+        response = self.app.\
+            post('/api/v1/parcels',
+                data=json.dumps(parcel),
+                headers=headers
+            )
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('Parcel created successfully', str(response.data))
