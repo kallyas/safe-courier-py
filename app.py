d49dae595d136ed755d760632a9c43ebd53b5762
kallyas <kallyasmedia@gmail.com>
2022-08-01 20:22:53 +0300

    fix: flask update

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from api import create_app, db, jwt
-from flask_script import Manager
+# from flask_script import Manager
 from flask_migrate import MigrateCommand
 
 
 app = create_app()
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     db.create_all(app=create_app())
